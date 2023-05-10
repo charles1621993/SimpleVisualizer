93ea47dd9fdeba2ab9a1ddfe7367a7e3b0261d5a
Charles Simson <charlesimanuvel@gmail.com>
2023-05-10 15:50:37 +0530

    Created first version of model

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "simvis.apps.SimvisConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
